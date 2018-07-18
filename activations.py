fe8ee75e94c44e2a5db4c5be617fdc5d4e82e1ff
Akshaynath R <akshaynathr@gmail.com>
2018-07-18 21:41:21 +0530

    all activation are added in this file.

---
+++ activations.py
@@ -0,0 +1,23 @@
+import numpy as np 
+
+
+class Sigmoid:
+    def forward(self,val):
+        return 1./(1.0+np.exp(-val) ) # SIGMOID FUNCTION
+
+    
+    def backward(self,val,diff_val):
+        output = self.forward(val)  # output is value of sigma(x)
+        return ((1.0 - output)*  output * diff_val  ) # derivative of SIGMOID FUNCTION --> sigma(x)* (1-sigma(x))
+
+
+
+
+class Tanh:
+    def forward(self,val):
+        return np.tanh(val)
+
+    def backward(self,val,diff_val):
+        output = self.forward(val)
+        return (1.0 - np.square(2)* diff_val)
+
